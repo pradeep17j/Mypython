ce8cf1c4c3f6da1781bf9bc5671f248b0fa67d3f
pradeep17j <pradeep17j@yahoo.com>
2015-04-07 19:45:58 -0700

    class changes

--- stats_collect.py
+++ stats_collect.py
@@ -14,12 +14,21 @@ class va_box_prompt:
         self.CLI_PROMPT1= re.compile('\S+\s*\(config\)\s*#')
 
 class va_box(va_box_prompt):
-    host_name=None
+    hostname=None
     shell=None
     cli=None
-    
+    def get_login(self):
+        login(self)
+        login(self,access='cli')
+    def exec_on_shell(self,cmd):
+        return exec_this_shell_cmd(self,cmd)
+    def exec_on_cli(self,cmd):
+        return exec_this_cli_cmd(self,cmd)
 
-    
+class loadgen_box(va_box):
+    cmd= '/var/loadgen_edge -f /var/cfg/loadgen-'
+    cmd= cmd+loadgen_num +'.xml'+' --test-duration 1500 --read-op --burst-rate 65536 --num-ops 2,100 &'
+    output=None
 
 
 cmd= 'ssh'
@@ -29,55 +38,51 @@ params= ''' -oStrictHostKeyChecking=no
     -oConnectTimeout=10 -oServerAliveInterval=300
     -oForwardAgent=no '''
 
-COMMAND_PROMPT = re.compile('\[admin\@\S+ \S+\]#')
-CLI_PROMPT = re.compile('\S+\s*(>|#|\(config\)\s*#)')
-CLI_PROMPT1 = re.compile('\S+\s*\(config\)\s*#')
+#systems=[ probe, loadgen, tarpon ]
 
-#Probe edge
-probe='oak-sh610'
-
-# Edge on which loadgen runs
-loadgen='oak-sh584'
-
-# Tarpon core
-tarpon='oak-sh809'
-
-systems=[ probe, loadgen, tarpon ]
-
-def login(hostname,access='shell'):
+def login(self,access='shell'):
+    hostname= self.hostname
     cmd1 = cmd + params + 'root@'+ hostname
     child= pexpect.spawn(cmd1)    
     child.logfile = sys.stdout
-    index= child.expect(['password',COMMAND_PROMPT,pexpect.EOF])
+    index= child.expect(['password',self.COMMAND_PROMPT,pexpect.EOF])
     if index==0:
         child.sendline( '2top90!')
-        child.expect(COMMAND_PROMPT)
+        child.expect(self.COMMAND_PROMPT)
     elif index!=1:
         print "Not able to login"
         sys.exit(0)
 
     if access is 'shell':
-        return child
+        self.shell= child
     elif access is 'cli':
         child.sendline('cli')
         child.expect('>')
         child.sendline('terminal length 0')
-        child.expect(CLI_PROMPT)
+        child.expect(self.CLI_PROMPT)
         child.sendline('en')
-        child.expect(CLI_PROMPT)
+        child.expect(self.CLI_PROMPT)
         child.sendline('con t')
-        child.expect(CLI_PROMPT1)
-        return child
+        child.expect(self.CLI_PROMPT1)
+        self.cli= child
 
 
-access_types={'shell':None,'cli':None}
-handles={}
-for i in systems:
-    dict1= access_types.copy()
-    for j in access_types.keys():
-        login_handle= login(i,access=j)
-        dict1[j]=login_handle
-    handles[i]=dict1
+#Probe edge
+probe= va_box()
+probe.hostname='oak-sh610'
+probe.get_login()
+
+# Edge on which loadgen runs
+loadgen= va_box()
+loadgen.hostname='oak-sh584'
+loadgen.get_login()
+
+# Tarpon core
+tarpon= va_box()
+tarpon.hostname='oak-sh809'
+tarpon.get_login()
+
+import pdb; pdb.set_trace()
 
 def get_output_only(output):
     out= output.split('\n')
@@ -90,21 +95,21 @@ def get_output_only(output):
     return out1
     
 
-def exec_this_cli_cmd(cmd,this_handle,get_input=False):
+def exec_this_cli_cmd(self,cmd,get_input=False):
     if get_input:
         cmd=raw_input("Enter the command to execute:")
-    this_handle.sendline(cmd)
-    this_handle.expect(CLI_PROMPT1,timeout=160)
-    output= this_handle.before
+    self.cli.sendline(cmd)
+    self.cli.expect(self.CLI_PROMPT1,timeout=160)
+    output= self.cli.before
     output1= get_output_only(output)
     return output1
 
-def exec_this_shell_cmd(cmd,this_handle,get_input=False):
+def exec_this_shell_cmd(self,cmd,get_input=False):
     if get_input:
         cmd=raw_input("Enter the command to execute:")
-    this_handle.sendline(cmd)
-    this_handle.expect(COMMAND_PROMPT,timeout=160)
-    output= this_handle.before
+    self.shell.sendline(cmd)
+    self.shell.expect(self.COMMAND_PROMPT,timeout=160)
+    output= self.shell.before
     output1= get_output_only(output)
     return output1
 
@@ -127,19 +132,18 @@ def start_loadgen():
 def get_stats():
     date_cmd= 'date +\'%F %T\''
 
-    tarpon_shell= handles[tarpon]['shell']
-    tarpon_cli= handles[tarpon]['cli']
-    probe_shell= handles[probe]['shell']
-    probe_cli= handles[probe]['cli'] 
-
-    core_start_time= exec_this_shell_cmd(date_cmd,tarpon_shell)
+    import pdb; pdb.set_trace()
+    core_start_time= tarpon.exec_on_shell(date_cmd)
     core_start_time[0]= "'"+core_start_time[0].replace('-','/')+"'"
-    edge_start_time= exec_this_shell_cmd(date_cmd,probe_shell)
+
+    edge_start_time= probe.exec_on_shell(date_cmd)
     edge_start_time[0]= "'"+edge_start_time[0].replace('-','/')+"'" 
+
     time.sleep(10)
-    core_end_time= exec_this_shell_cmd(date_cmd,tarpon_shell)
+
+    core_end_time= tarpon.exec_on_shell(date_cmd)
     core_end_time[0]= "'"+core_end_time[0].replace('-','/')+"'"
-    edge_end_time= exec_this_shell_cmd(date_cmd,probe_shell)
+    edge_end_time= probe.exec_on_shell(date_cmd)
     edge_end_time[0]= "'"+edge_end_time[0].replace('-','/')+"'"
 
     core_stats_cmd= "show stats storage filer-latency start-time "
